1ddf2735448e8410fddd2daa6f6a941c479ce52e
Jonathan Peck <peckjonathan@gmail.com>
2013-08-11 00:06:23 +0200

    updated model handling

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -1,9 +1,24 @@
 <?php
 class AdminController extends CController
 {
+	public function filters()
+    {
+        return array( 'accessControl' );
+    }
+
+	public function accessRules()
+    {
+        return array(
+            array('allow', // enkel ingelogde gebruikers kunnen overal aan
+                'users'=>array('@'),
+            ),
+            array('deny'),
+        );
+    }
+
 	public function actionUpdate()
 	{
-		$model = new User;
+		$model = new AdminModel;
 
 		$this->render("update", array("model" => $model));
 	}
