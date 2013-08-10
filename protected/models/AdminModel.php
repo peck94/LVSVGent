d7c7793bd73eff3ea18f7a58fb8c92e2af8fd3b6
Jonathan Peck <peckjonathan@gmail.com>
2013-08-11 00:39:29 +0200

    data saves correctly

--- protected/models/AdminModel.php
+++ protected/models/AdminModel.php
@@ -18,5 +18,37 @@ class AdminModel extends CFormModel
 			"confirm_password" => "Wachtwoord bevestigen",
 		);
 	}
+	
+	public function save()
+	{
+		// naam ophalen
+		$name = Yii::app()->user->name;
+		
+		// status bijhouden
+		$status = true;
+		
+		// controleer of de wachtwoorden overeenkomen
+		if($this->new_password !== $this->confirm_password){
+			$this->addError("confirm_password", "De wachtwoorden komen niet overeen.");
+			$status = false;
+		}
+		
+		// controleer of het wachtwoord juist is
+		$user = User::model()->findByAttributes(array("username" => $name));
+		if(md5($this->current_password) !== $user->password){
+			$this->addError("current_password", "Het wachtwoord is onjuist.");
+			$status = false;
+		}
+		
+		// als alles ok is
+		if($status){
+			// sla de data op
+			$user->password = md5($this->new_password);
+			$status = $user->save();
+		}
+		
+		// geef de status terug
+		return $status;
+	}
 }
 ?>
