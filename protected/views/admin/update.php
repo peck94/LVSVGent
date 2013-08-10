3fe5edf6ef242482752a1e0b59a49f2df1a1dafc
Jonathan Peck <peckjonathan@gmail.com>
2013-08-10 23:49:06 +0200

    implemented standard functionality

--- protected/views/admin/update.php
+++ protected/views/admin/update.php
@@ -1,1 +1,21 @@
 <h2>Wachtwoord wijzigen</h2>
+
+<?php echo CHtml::beginForm(); ?>
+
+<div class="form">
+	<div class="row">
+		<?php echo CHtml::activeLabel($model, "current_password"); ?>
+		<?php echo CHtml::activePasswordField($model, "current_password"); ?>
+	</div>
+
+	<div class="row">
+		<?php echo CHtml::activeLabel($model, "new_password"); ?>
+		<?php echo CHtml::activePasswordField($model, "new_password"); ?>
+	</div>
+
+	<div class="row submit">
+		<?php echo CHtml::submitButton("Opslaan"); ?>
+	</div>
+</div>
+
+<?php echo CHtml::endForm(); ?>
