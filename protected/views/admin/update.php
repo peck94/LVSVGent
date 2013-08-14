e715834769f468309932fd24778e338cdc6e9f65
Jonathan Peck <peckjonathan@gmail.com>
2013-08-14 19:35:44 +0200

    added fields

--- protected/views/admin/update.php
+++ protected/views/admin/update.php
@@ -1,14 +1,13 @@
-<h2>Wachtwoord wijzigen</h2>
+<h2>Gegevens wijzigen</h2>
 
 <p>
 <?php
 	if(isset($success)){
 		if($success){
-			echo "Het wachtwoord is aangepast.";
+			echo "De gegevens zijn aangepast.";
 		}
 	}else{
-		echo "Hier kan u het wachtwoord aanpassen voor deze account.<br/>
-		Merk op dat het hele bestuur van deze wijziging op de hoogte gebracht zal worden.";
+		echo "Merk op dat het hele bestuur van deze wijzigingen op de hoogte gebracht zal worden.";
 	}
 ?>
 </p>
@@ -19,17 +18,27 @@
 	<?php echo CHtml::errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo CHtml::activeLabel($model, "current_password"); ?>
+		<?php echo CHtml::activeLabelEx($model, "name"); ?>
+		<?php echo CHtml::activeTextField($model, "name"); ?>
+	</div>
+
+	<div class="row">
+		<?php echo CHtml::activeLabelEx($model, "email"); ?>
+		<?php echo CHtml::activeTextField($model, "email"); ?>
+	</div>
+
+	<div class="row">
+		<?php echo CHtml::activeLabelEx($model, "current_password"); ?>
 		<?php echo CHtml::activePasswordField($model, "current_password"); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabel($model, "new_password"); ?>
+		<?php echo CHtml::activeLabelEx($model, "new_password"); ?>
 		<?php echo CHtml::activePasswordField($model, "new_password"); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabel($model, "confirm_password"); ?>
+		<?php echo CHtml::activeLabelEx($model, "confirm_password"); ?>
 		<?php echo CHtml::activePasswordField($model, "confirm_password"); ?>
 	</div>
 
