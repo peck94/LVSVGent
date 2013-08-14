b16d8f9a4543ac1b478a912beedec7b5ed8ea816
Jonathan Peck <peckjonathan@gmail.com>
2013-08-14 17:08:21 +0200

    added edit link

--- protected/views/leden/view.php
+++ protected/views/leden/view.php
@@ -1,5 +1,11 @@
 <h2><?php echo $model->voornaam . " " . $model->achternaam ?></h2>
 
+<p>
+<?php
+	echo CHtml::link("Bewerken", array("leden/update", "id" => $model->id));
+?>
+</p>
+
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
