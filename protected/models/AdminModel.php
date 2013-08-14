901d1ec5e0ea8ed3503885e88813b05bba8f3b8b
Jonathan Peck <peckjonathan@gmail.com>
2013-08-14 19:35:10 +0200

    updated fields and submission

--- protected/models/AdminModel.php
+++ protected/models/AdminModel.php
@@ -1,12 +1,13 @@
 <?php
 class AdminModel extends CFormModel
 {
-	public $name, $current_password, $new_password, $confirm_password;
-	
+	public $name, $current_password, $new_password, $confirm_password, $email, $username;
+
 	public function rules()
 	{
 		return array(
-			array("current_password, new_password, confirm_password", "required"),
+			array("current_password, email", "required"),
+			array("name, confirm_password, new_password", "safe"),
 		);
 	}
 	
@@ -16,41 +17,68 @@ class AdminModel extends CFormModel
 			"current_password" => "Huidig wachtwoord",
 			"new_password" => "Nieuw wachtwoord",
 			"confirm_password" => "Wachtwoord bevestigen",
+			"email" => "E-mailadres",
+			"username" => "Gebruikersnaam",
+			"name" => "Naam",
 		);
+	}
+	
+	public function load()
+	{
+		$user = User::model()->findByPk(Yii::app()->user->id);
+		$this->username = $user->username;
+		$this->email = $user->email;
+		$this->name = $user->name;
 	}
 	
 	public function save()
 	{
-		// naam ophalen
-		$name = Yii::app()->user->name;
-		
 		// status bijhouden
 		$status = true;
+	
+		// valideer regels
+		if(!$this->validate()){
+			// fout
+			$status = false;
+		}
 		
-		// controleer of de wachtwoorden overeenkomen
-		if($this->new_password !== $this->confirm_password){
+		// haal gebruiker op
+		$user = User::model()->findByPk(Yii::app()->user->id);
+		
+		// controleer wachtwoorden
+		if($this->new_password != $this->confirm_password){
 			$this->addError("confirm_password", "De wachtwoorden komen niet overeen.");
 			$status = false;
 		}
 		
-		// controleer of het wachtwoord juist is
-		$user = User::model()->findByAttributes(array("username" => $name));
-		if(md5($this->current_password) !== $user->password){
+		// check huidig wachtwoord
+		if(User::encrypt($this->current_password) != $user->password){
 			$this->addError("current_password", "Het wachtwoord is onjuist.");
 			$status = false;
 		}
 		
-		// als alles ok is
-		if($status){
-			// sla de data op
-			$user->password = md5($this->new_password);
-			$status = $user->save();
-			
-			// verstuur mail
-			mail("peckjonathan@gmail.com", "Wijziging databasewachtwoord", "Het nieuwe wachtwoord is $this->new_password");
+		// check e-mail
+		if(preg_match("/[^ ]+@[^ ]+\.[^ ]{2,}/", $this->email) == 0){
+			$this->addError("email", "Ongeldig e-mailadres.");
+			$status = false;
 		}
 		
-		// geef de status terug
+		// check naam
+		if(preg_match("/^[a-zA-Z0-9]+$/", $this->username) == 0){
+			$this->addError("username", "Ongeldige gebruikersnaam.");
+			$status = false;
+		}
+		
+		// opslaan indien alles ok
+		if($status){
+			$user->email = $this->email;
+			if(!empty($this->new_password)){
+				$user->password = User::encrypt($this->new_password);
+			}
+			$user->username = $this->username;
+			$status = $user->save();
+		}
+
 		return $status;
 	}
 }
