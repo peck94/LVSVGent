9f3029a81854ab2cf767e8fbdd1e773fccb127b7
Jonathan Peck <peckjonathan@gmail.com>
2013-09-01 00:50:26 +0200

    updated contact info

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@
 		Gebruik het menu hiernaast om de site te navigeren.
 	</p>
 	<p>
-		Indien u uw gegevens kwijt bent, mail <a href="mailto:weblvsvgent@gmail.com">de webmaster</a>.
+		Indien u uw gegevens kwijt bent, mail <a href="mailto:webmaster@lvsvgent.be">de webmaster</a>.
 		<br/>
 		Ernstigere problemen kunt u melden bij <a href="mailto:peckjonathan@gmail.com">Jonathan Peck</a>.
 	</p>
